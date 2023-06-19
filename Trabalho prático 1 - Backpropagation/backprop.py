88ecd5414a89482fa4882057b2f5ecbbe741f7eb
Gabriel <60001504+gabrielpalves@users.noreply.github.com>
2023-06-19 12:53:22 -0300

    exc 3 e 4

--- Trabalho prático 1 - Backpropagation/backprop.py
+++ Trabalho prático 1 - Backpropagation/backprop.py
@@ -1,5 +1,4 @@
 import scipy.io
-import os
 import numpy as np
 from matplotlib import pyplot
 
@@ -45,55 +44,20 @@ def debugInitializeWeights(fan_out, fan_in):
     """
     Initialize the weights of a layer with fan_in incoming connections and fan_out outgoings
     connections using a fixed strategy. This will help you later in debugging.
-
-    Note that W should be set a matrix of size (1+fan_in, fan_out) as the first row of W handles
-    the "bias" terms.
-
-    Parameters
-    ----------
-    fan_out : int
-        The number of outgoing connections.
-
-    fan_in : int
-        The number of incoming connections.
-
-    Returns
-    -------
-    W : array_like (1+fan_in, fan_out)
-        The initialized weights array given the dimensions.
+    Returns the initialized weights array given the dimensions.
     """
-    # Initialize W using "sin". This ensures that W is always of the same values and will be
-    # useful for debugging
+    
+    # Initialize W using "sin"
+    # This ensures that W is always of the same values and will be useful for debugging
     W = np.sin(np.arange(1, 1 + (1+fan_in)*fan_out))/10.0
     W = W.reshape(fan_out, 1+fan_in, order='F')
     return W
 
 
-def computeNumericalGradient(J, theta, e=1e-4):
+def compute_numerical_gradient(J, theta, e=1e-4):
     """
-    Computes the gradient using "finite differences" and gives us a numerical estimate of the
-    gradient.
-
-    Parameters
-    ----------
-    J : func
-        The cost function which will be used to estimate its numerical gradient.
-
-    theta : array_like
-        The one dimensional unrolled network parameters. The numerical gradient is computed at
-         those given parameters.
-
-    e : float (optional)
-        The value to use for epsilon for computing the finite difference.
-
-    Notes
-    -----
-    The following code implements numerical gradient checking, and
-    returns the numerical gradient. It sets `numgrad[i]` to (a numerical
-    approximation of) the partial derivative of J with respect to the
-    i-th input argument, evaluated at theta. (i.e., `numgrad[i]` should
-    be the (approximately) the partial derivative of J with respect
-    to theta[i].)
+    Computes the gradient using "finite differences"
+    and gives a numerical estimate of the gradient.
     """
     numgrad = np.zeros(theta.shape)
     perturb = np.diag(e * np.ones(theta.shape))
@@ -104,20 +68,12 @@ def computeNumericalGradient(J, theta, e=1e-4):
     return numgrad
 
 
-def checkNNGradients(nnCostFunction, activation_function):
+def check_NN_gradients(nn_cost_function, activation_function):
     """
     Creates a small neural network to check the backpropagation gradients. It will output the
-    analytical gradients produced by your backprop code and the numerical gradients
-    (computed using computeNumericalGradient). These two gradient computations should result in
+    analytical gradients produced by the backpropagation code and the numerical gradients
+    (computed using compute_numerical_gradient). These two gradient computations should result in
     very similar values.
-
-    Parameters
-    ----------
-    nnCostFunction : func
-        A reference to the cost function implemented by the student.
-
-    lambda_ : float (optional)
-        The regularization parameter value.
     """
     input_layer_size = 3
     hidden_layer_size = 5
@@ -136,17 +92,17 @@ def checkNNGradients(nnCostFunction, activation_function):
     nn_params = np.concatenate([Theta1.ravel(), Theta2.ravel()])
 
     # short hand for cost function
-    def costFunc(p): return nnCostFunction(p,
+    def costFunc(p): return nn_cost_function(p,
                                            num_labels, X, y, activation_function, layer_size=[input_layer_size, hidden_layer_size, num_labels])
     cost, grad = costFunc(nn_params)
-    numgrad = computeNumericalGradient(costFunc, nn_params)
+    numgrad = compute_numerical_gradient(costFunc, nn_params)
 
     # Visually examine the two gradient computations.The two columns you get should be very similar.
     print('The above two columns you get should be very similar.')
     print('(Left-Your Numerical Gradient, Right-Analytical Gradient)\n')
 
     # Evaluate the norm of the difference between two the solutions. If you have a correct
-    # implementation, and assuming you used e = 0.0001 in computeNumericalGradient, then diff
+    # implementation, and assuming you used e = 0.0001 in compute_numerical_gradient, then diff
     # should be less than 1e-9.
     diff = np.linalg.norm(numgrad - grad)/np.linalg.norm(numgrad + grad)
 
@@ -156,8 +112,7 @@ def checkNNGradients(nnCostFunction, activation_function):
 
 
 
-
-
+# Activation functions
 def sigmoid(z):
     """Activation function: Sigmoid"""
     s = lambda z: 1.0 / (1.0 + np.exp(-z))
@@ -177,6 +132,18 @@ def relu(z):
     d[z <= 0] = 0
     return np.maximum(np.zeros(z.shape), z), d
 
+
+def set_activation_function(activation_function):
+    match activation_function:
+        case 'sigmoid':
+            activation_function = sigmoid
+        case 'tanh':
+            activation_function = tanh
+        case 'relu':
+            activation_function = relu
+        case _:
+            activation_function = sigmoid
+    return activation_function
 
 
 def all_activations(X, layer_size, nn_params, activation_function):
@@ -211,34 +178,15 @@ def all_activations(X, layer_size, nn_params, activation_function):
     return activations, nets
 
 
-def nnCostFunction(nn_params, num_labels, X, y, activation_function=sigmoid, layer_size=[400, 25, 10]):
+def nn_cost_function(nn_params, num_labels, X, y, activation_function, layer_size):
     """
-    Implements the neural network cost function and gradient for a two layer neural 
+    Implements the neural network cost function and gradient for a neural 
     network which performs classification. 
 
-    Parameters
-    ----------
-    nn_params : array_like
-        The parameters for the neural network which are "unrolled" into 
-        a vector. This needs to be converted back into the weight matrices Thetas.
-
-    num_labels : int
-        Total number of labels, or equivalently number of units in output layer. 
-
-    X : array_like
-        Input dataset. A matrix of shape (m x input_layer_size).
-
-    y : array_like
-        Dataset labels. A vector of shape (m,).
-
     Returns
-    -------
-    J : float
-        The computed value for the cost function at the current weight values.
-
-    grad : array_like
-        An "unrolled" vector of the partial derivatives of the concatenation of
-        neural network weights Thetas.
+        cost J for the current weights and
+        grad, an "unrolled" vector of the partial derivatives of the concatenation of
+    neural network weights Thetas.
     """
 
     # Reshape nn_params
@@ -279,26 +227,7 @@ def nnCostFunction(nn_params, num_labels, X, y, activation_function=sigmoid, lay
 
 def randInitializeWeights(L_in, L_out, epsilon_init=0):
     """
-    Randomly initialize the weights of a layer in a neural network.
-
-    Parameters
-    ----------
-    L_in : int
-        Number of incoming connections.
-
-    L_out : int
-        Number of outgoing connections. 
-
-    epsilon_init : float, optional
-        Range of values which the weight can take from a uniform 
-        distribution.
-
-    Returns
-    -------
-    W : array_like
-        The weight initialized to random values.  Note that W should
-        be set to a matrix of size(L_out, 1 + L_in) as
-        the first column of W handles the "bias" terms.
+    Randomly initialize the weights of a layer in a neural network
     """
 
     if epsilon_init == 0:
@@ -318,7 +247,7 @@ def gradient(i, learning_rate, theta, X_train, X_val, y_train, y_val, layer_size
     m = y.shape[0]
     num_labels = np.unique(y).shape[0]
 
-    J, grad = nnCostFunction(theta,
+    J, grad = nn_cost_function(theta,
                              num_labels,
                              X_train, y_train, activation_function=activation_function, layer_size=layer_size)
 
@@ -328,11 +257,11 @@ def gradient(i, learning_rate, theta, X_train, X_val, y_train, y_val, layer_size
         acc_train = predict(X_train, y_train, layer_size, theta, activation_function, 'Training')
         acc_val = predict(X_val, y_val, layer_size, theta, activation_function, 'Validation')
         
-        J, grad = nnCostFunction(theta,
+        J, grad = nn_cost_function(theta,
                                  num_labels,
                                  X_train, y_train, activation_function=activation_function, layer_size=layer_size)
         
-        J_val, _ = nnCostFunction(theta,
+        J_val, _ = nn_cost_function(theta,
                                  num_labels,
                                  X_val, y_val, activation_function=activation_function, layer_size=layer_size)
 
@@ -345,17 +274,14 @@ def gradient(i, learning_rate, theta, X_train, X_val, y_train, y_val, layer_size
     return theta, theta_history, J_history, J_hist_val, acc_hist_train, acc_hist_val
 
 
-def run_neural_network(X, y, layer_size, activation_function):
-    # Divide in training, validation and test
-    training_set = 0.70
-    validation_set = 0.20
-    # test_set = 1 - (training_set + validation_set)
+def run_neural_network(X, y, layer_size, activation_function='sigmoid', epochs=500, learning_rate=1, training_set=0.80, validation_set=0.10):
+    """Function that runs the neural network implemented to solve the problem"""
     
+    # Divide in training, validation and test
     m = y.shape[0]
     
-    # Shuffle
     rng = np.random.default_rng()
-    idx = rng.permutation(m)
+    idx = rng.permutation(m) # Shuffle
     
     X = X[idx, :]
     y = y[idx]
@@ -373,15 +299,7 @@ def run_neural_network(X, y, layer_size, activation_function):
     y_test = y[n_train+n_val:]
     
     # Set activation function
-    match activation_function:
-        case 'sigmoid':
-            activation_function = sigmoid
-        case 'tanh':
-            activation_function = tanh
-        case 'relu':
-            activation_function = relu
-        case _:
-            activation_function = sigmoid
+    activation_function = set_activation_function(activation_function)
     
     # Thetas unrolled
     initial_nn_params = np.array([])
@@ -390,11 +308,9 @@ def run_neural_network(X, y, layer_size, activation_function):
             layer_size[i], layer_size[i+1]).ravel()), axis=0)
     
     # Check neural network gradients
-    # checkNNGradients(nnCostFunction, activation_function)
+    # check_NN_gradients(nn_cost_function, activation_function)
     
     # Gradient descent
-    epochs = 5000
-    learning_rate = 2
     theta, theta_history, J_hist_train, J_hist_val, acc_hist_train, acc_hist_val = gradient(
         epochs, learning_rate, initial_nn_params, X_train, X_val, y_train, y_val, layer_size, activation_function
         )
@@ -406,8 +322,8 @@ def run_neural_network(X, y, layer_size, activation_function):
 
     
     # Plot accuracy (training and validation)
-    pyplot.rcParams.update({'font.size': 48})
-    pyplot.figure(figsize=(16,9))
+    pyplot.rcParams.update({'font.size': 35})
+    pyplot.figure(figsize=(16, 11))
     pyplot.plot(np.arange(1, epochs+1), acc_hist_train, 'b-o', linewidth=2)
     pyplot.plot(np.arange(1, epochs+1), acc_hist_val, 'r-o', linewidth=2)
     pyplot.xlabel('Épocas')
@@ -416,16 +332,18 @@ def run_neural_network(X, y, layer_size, activation_function):
     pyplot.grid(visible=True, which='both')
     pyplot.yticks(ticks=np.arange(0, 101, 10))
     pyplot.title(f'Taxa de aprendizado: {learning_rate}')
+    pyplot.savefig(f'accuracy_{J_hist_train.shape[0]}epochs_{learning_rate}alpha.png', dpi=300)
     
     # Plot loss (training and validation)
-    pyplot.figure(figsize=(16,9))
+    pyplot.figure(figsize=(16, 11))
     pyplot.plot(np.arange(1, epochs+1), J_hist_train, 'b-o', linewidth=2)
     pyplot.plot(np.arange(1, epochs+1), J_hist_val, 'r-o', linewidth=2)
     pyplot.xlabel('Épocas')
     pyplot.ylabel('Custo')
-    pyplot.legend(['Custo de treinamento', 'Custo de validação'])
+    pyplot.legend(['Custo de treinamento', 'Custo de validação'])   
     pyplot.grid(visible=True, which='both')
     pyplot.title(f'Taxa de aprendizado: {learning_rate}')
+    pyplot.savefig(f'cost_{J_hist_train.shape[0]}epochs_{learning_rate}alpha.png', dpi=300)
     
     return theta
 
@@ -440,14 +358,14 @@ X, y = data[:,:-1], data[:,-1].ravel().astype(int)
 # y[y == 10] = 0
 
 # Inputs
-X = X
-y = y
-layer_size = [X.shape[1], 50, np.unique(y).shape[0]]
+layer_size = [X.shape[1], 8, 8, np.unique(y).shape[0]]
 activation_function = 'sigmoid'
+training_set = 0.80
+validation_set = 0.10
+epochs = 500
+learning_rate = 2
 
-theta = run_neural_network(X, y, layer_size, activation_function)
-
-
+theta = run_neural_network(X, y, layer_size, activation_function, epochs, learning_rate, training_set, validation_set)
 
 
 
@@ -464,71 +382,64 @@ Original file is located at
 ##### Plotando fronteira de decisão não-linear
 ###############
 
-
-
 import matplotlib.pyplot as plt
 
-# Plotando fronteira de decisão
-x1s = np.linspace(-1,1.5,200)
-x2s = np.linspace(-1,1.5,200)
-z=np.zeros((len(x1s),len(x2s)))
+if X.shape[1] == 2:
+    # Plotando fronteira de decisão
+    x1s = np.linspace(-1,1.5,200)
+    x2s = np.linspace(-1,1.5,200)
+    z=np.zeros((len(x1s),len(x2s)))
 
 
-# Set activation function
-match activation_function:
-    case 'sigmoid':
-        activation_function = sigmoid
-    case 'tanh':
-        activation_function = tanh
-    case 'relu':
-        activation_function = relu
-    case _:
-        activation_function = sigmoid
+    # Set activation function
+    activation_function = set_activation_function(activation_function)
 
-for i in range(len(x1s)):
-    for j in range(len(x2s)):
-        # x = np.array([x1s[i], x2s[j]]).reshape(2,-1)
-        # z[i,j] = net_z_output( x )  # saida do modelo antes de aplicar a função sigmoide - substituir aqui teu código
-        x = np.array([[x1s[i], x2s[j]]])
-        _, nets = all_activations(x, layer_size, theta, activation_function)
-        zzz = nets[-1]
-        z[i,j] = np.sum(zzz)
-pyplot.rcParams.update({'font.size': 48})
-plt.figure(figsize=(16,9))
-plt.contour(x1s,x2s,z.T,0)
-plt.xlabel("x1")
-plt.ylabel("x2")
-plt.legend(loc=0)
+    for i in range(len(x1s)):
+        for j in range(len(x2s)):
+            # x = np.array([x1s[i], x2s[j]]).reshape(2,-1)
+            # z[i,j] = net_z_output( x )  # saida do modelo antes de aplicar a função sigmoide - substituir aqui teu código
+            x = np.array([[x1s[i], x2s[j]]])
+            _, nets = all_activations(x, layer_size, theta, activation_function)
+            zzz = nets[-1]
+            z[i,j] = np.sum(zzz)
+    pyplot.rcParams.update({'font.size': 48})
+    plt.figure(figsize=(16,9))
+    plt.contour(x1s,x2s,z.T,0)
+    plt.xlabel("x1")
+    plt.ylabel("x2")
+    plt.legend(loc=0)
 
-###############
-##### Classificação binária com modelo de rede neural - backprop / regressão logística
-###############
+    ###############
+    ##### Classificação binária com modelo de rede neural - backprop / regressão logística
+    ###############
 
-import pandas as pd
-df=pd.read_csv("Trabalho prático 1 - Backpropagation/classification2.txt", header=None)
+    import pandas as pd
+    df=pd.read_csv("Trabalho prático 1 - Backpropagation/classification2.txt", header=None)
 
-X=df.iloc[:,:-1].values
-y=df.iloc[:,-1].values
-pos , neg = (y==1).reshape(118,1) , (y==0).reshape(118,1)
-plt.scatter(X[pos[:,0],0],X[pos[:,0],1],c="r",marker="+")
-plt.scatter(X[neg[:,0],0],X[neg[:,0],1],marker="o",s=10)
-plt.xlabel("x1")
-plt.ylabel("x2")
-plt.legend(["Accepted","Rejected"],loc=0)
+    X=df.iloc[:,:-1].values
+    y=df.iloc[:,-1].values
+    pos , neg = (y==1).reshape(118,1) , (y==0).reshape(118,1)
+    plt.scatter(X[pos[:,0],0],X[pos[:,0],1],c="r",marker="+")
+    plt.scatter(X[neg[:,0],0],X[neg[:,0],1],marker="o",s=10)
+    plt.xlabel("x1")
+    plt.ylabel("x2")
+    plt.legend(["Accepted","Rejected"],loc=0)
 
-###############
-##### Classificação de dígitos com modelo de rede neural - backprop
-###############
+if X.shape[1] > 2:
+    ###############
+    ##### Classificação de dígitos com modelo de rede neural - backprop
+    ###############
 
-from scipy.io import loadmat
-mat=loadmat("Trabalho prático 1 - Backpropagation/classification3.mat")
-X=mat["X"]
-y=mat["y"]
+    from scipy.io import loadmat
+    mat=loadmat("Trabalho prático 1 - Backpropagation/classification3.mat")
+    X=mat["X"]
+    y=mat["y"]
 
-import matplotlib.image as mpimg
-fig, axis = plt.subplots(10,10,figsize=(8,8))
-for i in range(10):
-    for j in range(10):
-        axis[i,j].imshow(X[np.random.randint(0,5001),:].reshape(20,20,order="F"), cmap="hot") #reshape back to 20 pixel by 20 pixel
-        axis[i,j].axis("off")
-print('fim')
+    import matplotlib.image as mpimg
+    fig, axis = plt.subplots(10,10,figsize=(8,8))
+    for i in range(10):
+        for j in range(10):
+            axis[i,j].imshow(X[np.random.randint(0,5001),:].reshape(20,20,order="F"), cmap="hot") #reshape back to 20 pixel by 20 pixel
+            axis[i,j].axis("off")
+
+print('FIM')
